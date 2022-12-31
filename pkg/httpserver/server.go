76ef65bb17484c494bc11345a2ea1ebbaddb6d0f
wasawatp <wasawatpruthipunyaskuk@Wasawats-MacBook-Pro-2.local>
2022-12-31 13:03:53 +0700

    add echo for http server

---
+++ pkg/httpserver/server.go
@@ -0,0 +1,45 @@
+package httpserver
+
+import (
+	"context"
+	"net"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	_defaultAddr     = ":80"
+	_shutdownTimeout = 3 * time.Second
+)
+
+type Server struct {
+	server          *echo.Echo
+	port            string
+	notify          chan error
+	shutdownTimeout time.Duration
+}
+
+func New(echo *echo.Echo, port string) *Server {
+	s := &Server{
+		server:          echo,
+		port:            _defaultAddr,
+		notify:          make(chan error, 1),
+		shutdownTimeout: _shutdownTimeout,
+	}
+	if port != "" {
+		s.port = net.JoinHostPort("", port)
+	}
+	s.start(port)
+	return s
+}
+
+func (s *Server) start(port string) {
+	s.server.Logger.Fatal(s.server.Start(s.port))
+}
+
+func (s *Server) Shutdown() error {
+	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+	return s.server.Shutdown(ctx)
+}
