3ef3c32ffc2d109d83c7b91be7aa28507c4fc51c
wasawatp <wasawatpruthipunyaskuk@Wasawats-MacBook-Pro-2.local>
2023-01-02 16:49:15 +0700

    graceful shutdown when httpServer error and process killed

--- server.go
+++ server.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 	"github.com/wasawaz/assessment/pkg/httpserver"
@@ -23,6 +25,23 @@ func main() {
 	e.GET("/healthz", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	httpserver.New(e, appPort)
+	httpServer := httpserver.New(e, appPort)
+
+	// Waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		log.Print("app - Run - signal: " + s.String())
+	case err = <-httpServer.Notify():
+		log.Fatalf("app - Run - httpServer.Notify: %v", err)
+	}
+
+	// Shutdown
+	err = httpServer.Shutdown()
+	if err != nil {
+		log.Fatalf("app - Run - httpServer.Shutdown: %v", err)
+	}
 
 }
