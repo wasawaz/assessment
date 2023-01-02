72a636d8815e251b73d2d31f1b22338e7597a18a
wasawatp <wasawatpruthipunyaskuk@Wasawats-MacBook-Pro-2.local>
2023-01-02 16:10:07 +0700

    add postgresql

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/labstack/echo/v4 v4.10.0
 
 require (
 	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lib/pq v1.10.7
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- server.go
+++ server.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/wasawaz/assessment/pkg/httpserver"
+	"github.com/wasawaz/assessment/pkg/postgresql"
 )
 
 func main() {
+	databaseConnectionString := os.Getenv("DATABASE_URL")
+	pg, err := postgresql.New(databaseConnectionString)
+	if err != nil {
+		log.Fatalf("cannot connect to db cause %v", err)
+	}
+	defer pg.Close()
+
 	appPort := os.Getenv("PORT")
 	e := echo.New()
 	e.GET("/healthz", func(c echo.Context) error {
