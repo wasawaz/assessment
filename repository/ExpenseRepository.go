dc2de0ba069d8f657c21f76219b59e66b43c2b0c
wasawatp <wasawatpruthipunyaskuk@Wasawats-MacBook-Pro-2.local>
2023-01-03 23:05:03 +0700

    add func Add for insert new expense

--- repository/ExpenseRepository.go
+++ repository/ExpenseRepository.go
@@ -1,7 +1,33 @@
 package repository
 
+import (
+	"github.com/lib/pq"
+	"github.com/wasawaz/assessment/entity"
+	"github.com/wasawaz/assessment/pkg/postgresql"
+)
+
 type IExpenseRepository interface {
+	Add(entity *entity.Expense) error
 }
 
-type ExpenseRepository struct {
+type expenseRepository struct {
+	dbContext *postgresql.PostgresqlDB
+}
+
+func NewExpenseRepository(dbContext *postgresql.PostgresqlDB) *expenseRepository {
+	return &expenseRepository{dbContext}
+}
+
+func (e *expenseRepository) Add(entity *entity.Expense) error {
+	stmt, err := e.dbContext.Db.Prepare(`INSERT INTO EXPENSES(TITLE, AMOUNT, NOTE, TAGS) VALUES($1, $2, $3, $4) RETURNING id`)
+	if err != nil {
+		return err
+	}
+	var id int
+	err = stmt.QueryRow(entity.Title, entity.Amount, entity.Note, pq.Array(&entity.Tags)).Scan(&id)
+	if err != nil {
+		return err
+	}
+	entity.Id = id
+	return nil
 }
