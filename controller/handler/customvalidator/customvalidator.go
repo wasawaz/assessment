31758995b373dc6e4fad119e772ffcce4feff72f
wasawatp <wasawatpruthipunyaskuk@Wasawats-MacBook-Pro-2.local>
2023-01-07 11:23:13 +0700

    documenting handlers

--- controller/handler/customvalidator/customvalidator.go
+++ controller/handler/customvalidator/customvalidator.go
@@ -1,17 +1,20 @@
+// Package customvalidator contains functions for validate struct of http request payload
+// This use with echo framework
 package customvalidator
 
 import (
 	"github.com/go-playground/validator"
 )
 
+// expenseValidator is used to assign echo validator which implement from go-playground/validator
 type expenseValidator struct {
 	validator *validator.Validate
 }
-
+// NewCustomValidator is used to create new expenseValidator validator
 func NewCustomValidator(validator *validator.Validate) *expenseValidator {
 	return &expenseValidator{validator}
 }
-
+// Validate is implement of go-playground/validator.validate function
 func (cv *expenseValidator) Validate(i interface{}) error {
 	if err := cv.validator.Struct(i); err != nil {
 		// Optionally, you could return the error to give each route more control over the status code
--- controller/handler/getAllExpenseHandler.go
+++ controller/handler/getAllExpenseHandler.go
@@ -1,3 +1,4 @@
+// Package handler contains functions for http request handling separate route name
 package handler
 
 import (
@@ -7,14 +8,17 @@ import (
 	"github.com/wasawaz/assessment/usecase"
 )
 
+// GetAllExpenseHandler is used for get all expense handler
 type GetAllExpenseHandler struct {
 	getAllExpenseUsecase usecase.IGetAllExpenseUsecase
 }
 
+// NewGetAllExpenseHandler is used for new GetAllExpenseHandler instance
 func NewGetAllExpenseHandler(getAllExpenseUsecase usecase.IGetAllExpenseUsecase) *GetAllExpenseHandler {
 	return &GetAllExpenseHandler{getAllExpenseUsecase}
 }
 
+// GetAllExpense is used for handle http request for get all expense
 func (e *GetAllExpenseHandler) GetAllExpense(c echo.Context) error {
 	expenses, err := e.getAllExpenseUsecase.Execute()
 	if err != nil {
